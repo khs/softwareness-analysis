81f0c461f7bb51fdf107abd9e37ad2c1b5533d9a
Keller Scholl <keller.scholl@gmail.com>
2017-08-24 16:10:27 -0400

    Created Ipython Notebook
    Created an Ipython notebook for performing and displaying analysis.

--- industry_dispersion.py
+++ industry_dispersion.py
@@ -2,5 +2,7 @@
 # Also sum for each industry.
 # For the first attempt, just ignore the non-national data.
 # Descending order of size: Major, Minor, Broad, Detail.
+import csv
 with open("test_data.csv",'r') as f:
+    
     pass
