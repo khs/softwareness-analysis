88e0142a4bc7cd40a6c4bcf071253eecd4213f0a
Keller Scholl <keller.scholl@gmail.com>
2017-08-24 09:40:21 -0400

    Saving test data.

---
+++ industry_dispersion.py
@@ -0,0 +1,6 @@
+# Each line after the first is either broad, medium, or narrow.
+# Also sum for each industry.
+# For the first attempt, just ignore the non-national data.
+# Descending order of size: Major, Minor, Broad, Detail.
+with open("test_data.csv",'r') as f:
+    pass
